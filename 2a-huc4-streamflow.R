b48d7a15009bfedfedd7c7f2b2b75b6cfe5f2313
Cameron Bracken <32280+cameronbracken@users.noreply.github.com>
2025-11-10 18:32:10 -0800

    Bug fix for badly formatted HUC4 codes on about 150 plants

--- 2a-huc4-streamflow.R
+++ 2a-huc4-streamflow.R
@@ -3,41 +3,13 @@ library(dataRetrieval)
 library(missRanger)
 
 end_year = 2024
-eha_fn <- "data/ORNL_EHAHydroPlant_PublicFY2024.xlsx"
-hilarri_fn <- "data/HILARRI_v1_1/HILARRI_v1_1_Public_SubsetHydropowerDams_Plants.csv"
-
-# read in HILARRI database
-read_csv(hilarri_fn, show = F, progress = F) %>%
-  filter(!is.na(eha_ptid)) ->
-HILARRI
-
-readxl::read_xlsx(eha_fn, sheet = "Operational") -> EHA
 
 # associate EIA plant code with HUC4
-EHA %>%
-  filter(
-    !EHA_PtID %in% HILARRI$eha_ptid,
-    !is.na(HUC)
-  ) %>%
-  mutate(HUC = if_else(nchar(HUC) == 11, paste0("0", HUC), as.character(HUC))) %>%
-  filter(nchar(HUC) == 12) %>%
-  mutate(HUC4 = substr(HUC, 1, 4)) %>%
-  select(eha_ptid = EHA_PtID, HUC4) %>%
-  unique() ->
-additional_HUC
+# use Dan's EHA POD dataset EHA and HILLARI have a lot of issues 
+# run this script to regenerate the eia_huc4.csv file
+# source('data/huc4_plants.R')
+EIA_and_HUC4 <- read_csv('data/eia_huc4.csv')
 
-# associate EIA plant code with HUC4
-HILARRI %>%
-  mutate(HUC4 = substr(huc_12, 1, 4)) %>%
-  select(eha_ptid, HUC4) %>%
-  bind_rows(additional_HUC) %>%
-  left_join(EHA %>% select(EHA_PtID, EIA_ID = EIA_PtID), by = c("eha_ptid" = "EHA_PtID")) %>%
-  filter(!is.na(EIA_ID)) %>%
-  select(HUC4, EIA_ID) %>%
-  unique() ->
-EIA_and_HUC4
-
-write_csv(EIA_and_HUC4, "data/eia_huc4.csv")
 
 ## CB Oct 2023, had to change line 140 from Data/USGS_Streamgage_HUC4.csv
 ## 05587455 to 05587450, the previous was not returning streamflow data
